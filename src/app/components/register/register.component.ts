1ae20c359361c18c90630259d05c8352b0e69d4a
Juan Sebastian Cardona Morales <juan.cardona@yuxiglobal.com>
2018-10-10 16:06:07 -0500

    responsive terminado para header, register y dashboard component.

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  days: number[] = new Array<number>();
+  months: string[] = new Array<string>();
+  years: number[] = new Array<number>();
 
   constructor() { }
 
   ngOnInit() {
+    for(let i = 1; i <= 31; i++){
+      this.days.push(i);
+    }
+    this.months = [
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre',
+    ];
+    for(let j = 1967; j <= 2019; j++){
+      this.years.push(j);
+    }
   }
-
 }
