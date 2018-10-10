8153a69c01e6b531996884bf122eb386cbf32e03
Alejandro <alejito.matallana@gmail.com>
2018-10-09 20:49:23 -0500

    Hello page footer Avances

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {MatRadioModule} from '@angular/material/radio';
+import { MatRadioModule } from '@angular/material/radio';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { SlideshowModule } from 'ng-simple-slideshow';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
@@ -10,24 +11,20 @@ import { RegisterComponent } from './components/register/register.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HelloPageComponent } from './components/hello-page/hello-page.component';
 import { CardComponent } from './components/card/card.component';
-
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    DashboardComponent,
-    HelloPageComponent,
-    CardComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    MatRadioModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LoginComponent,
+		RegisterComponent,
+		DashboardComponent,
+		HelloPageComponent,
+		CardComponent,
+		FooterComponent
+	],
+	imports: [ BrowserModule, AppRoutingModule, MatRadioModule, FormsModule, SlideshowModule ],
+	providers: [],
+	bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/hello-page/hello-page.component.ts
+++ src/app/components/hello-page/hello-page.component.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SlideshowModule, IImage } from 'ng-simple-slideshow';
 
 @Component({
-  selector: 'app-hello-page',
-  templateUrl: './hello-page.component.html',
-  styleUrls: ['./hello-page.component.css']
+	selector: 'app-hello-page',
+	templateUrl: './hello-page.component.html',
+	styleUrls: [ './hello-page.component.css' ]
 })
 export class HelloPageComponent implements OnInit {
+	images: Array<{ src: string; name: string }>;
 
-  constructor() { }
+	constructor(private router: Router) {
+		this.images = [
+			{
+				src:
+					'https://cinemarkla.modyocdn.com/uploads/09c4b3aa-12d4-4686-8c3e-4060dff9cdfe/original/ico-facebook.png',
+				name: 'Facebook'
+			},
+			{
+				src:
+					'https://cinemarkla.modyocdn.com/uploads/aeb3d7b0-1ce1-4208-b56d-868f19117142/original/ico-twitter.png',
+				name: 'Twitter'
+			},
+			{
+				src:
+					'https://cinemarkla.modyocdn.com/uploads/8109e80b-4302-484d-8877-e935545bf653/original/ico-instagram.png',
+				name: 'Instagram'
+			}
+		];
+	}
 
-  ngOnInit() {
-  }
+	imageUrls: (string | IImage)[] = [
+		{
+			url: 'https://www.cinemacenter.com.ar/img_movies/1814_img1.jpg',
+			caption: 'The first slide',
+			href: '#config'
+		},
+		{
+			url: 'http://www.cinespaseoaldrey.com.ar/images/bannervalamonja.jpg',
+			clickAction: () => alert('custom click function')
+		},
+		{
+			url:
+				'https://s3-eu-west-1.amazonaws.com/disasterdraw.blindcat/destacados/000/001/369/original/venom.jpg?1538699723',
+			caption: 'Apple TV',
+			href: 'https://www.apple.com/'
+		},
+		'https://i.ytimg.com/vi/RYQmtI-uz38/maxresdefault.jpg',
+		{ url: 'assets/kitties.jpg', backgroundSize: 'contain', backgroundPosition: 'center' }
+	];
+	height = '400px';
+	minHeight: string;
+	arrowSize = '30px';
+	showArrows = true;
+	disableSwiping = false;
+	autoPlay = true;
+	autoPlayInterval = 3333;
+	stopAutoPlayOnSlide = true;
+	debug = false;
+	backgroundSize = 'cover';
+	backgroundPosition = 'center center';
+	backgroundRepeat = 'no-repeat';
+	showDots = true;
+	dotColor = '#FFF';
+	showCaptions = true;
+	captionColor = '#FFF';
+	captionBackground = 'rgba(0, 0, 0, .35)';
+	lazyLoad = false;
+	hideOnNoSlides = false;
+	width = '100%';
 
+	ngOnInit() {}
 }
